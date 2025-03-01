a71285e31a701d08ff14493f8b97efc60079e3da
KhanhDu01 <d22tv@unb.ca>
2025-03-01 15:00:40 -0400

    Created basic sign up, sign in, home page
    Used firebase as the database and made sure users can only sign up with valid emails and password, sign in with emails and password already created.

---
+++ app/SignIn.js
@@ -0,0 +1,30 @@
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+import {initializeApp} from 'firebase/app';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAvf-gvB_eXWSYcRytg052UAqQI7XTgBNY",
+    authDomain: "languageapp-5fa29.firebaseapp.com",
+    projectId: "languageapp-5fa29",
+    storageBucket: "languageapp-5fa29.firebasestorage.app",
+    messagingSenderId: "672634105164",
+    appId: "1:672634105164:web:73bf5dbb2a0e1b4ae7f3e9",
+    measurementId: "G-20CR15B4WH"
+};
+
+// Initialize firebase
+const app = initializeApp(firebaseConfig);
+
+// Set up authenticator
+const auth = getAuth(app);
+
+export async function signIn(email, pw){
+
+    try {
+        await signInWithEmailAndPassword(auth, email, pw)
+        return false;
+    } catch(error) {
+        return true;
+    };
+    
+}
