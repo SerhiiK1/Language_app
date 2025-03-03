f516c6ddc7aebacac661008ddad82f23c202850e
KhanhDu01 <d22tv@unb.ca>
2025-03-03 15:33:20 -0400

    User can create individual cards in a card set

--- app/pages/cards/CreateCardSet.js
+++ app/pages/cards/CreateCardSet.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import {View, Text, TouchableOpacity, Modal, Pressable, StyleSheet, Dimensions} from 'react-native'
+import React, { useState } from 'react';
+import {View, ScrollView, Text, TextInput, Modal, Pressable, StyleSheet, Dimensions} from 'react-native'
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context'
 import {CardSet} from './Card'
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
+const NewCard = (id) => {
+    const key = id
+    const [front, setFront] = useState('')
+    const [back, setBack] = useState('')
+
+    return(
+        <View style = {styles.newCardStyle}>
+            <TextInput placeholder='Front' 
+                style={styles.textInputStyle}
+                onChangeText={setFront}/>
+
+
+            <TextInput placeholder='Back' 
+                style={styles.textInputStyle}
+                onChangeText={setBack}/>
+
+            <Pressable style={styles.deleteStyle}
+                onPress={() => {}}>
+                <Text>Delete</Text>
+            </Pressable>
+        </View>
+    )
+}
+
 const CreateCardSet = ({navigation, visible, setVisible}) => {
-    
+    const [hoveredButton, setHoveredButton] = useState(null);
+    const [cardSet, setCardSet] = useState([])
+    const [id, setId] = useState(0)
+
     return(
         <SafeAreaProvider>
             <SafeAreaView>
@@ -22,24 +49,47 @@ const CreateCardSet = ({navigation, visible, setVisible}) => {
                     <View style = {styles.modalContainer}>
                         <View style = {styles.modalView}>
                             <View style = {styles.modalContent}>
-                                <TouchableOpacity
-                                    onPress={() => {
+                                <Text>Enter the name of the card set</Text>
+                                {cardSet}
+                            </View>
+                            <View style = {styles.modalBottom}>
+                                <Pressable 
+                                    onPress={() => {setVisible(!visible)}}
+                                    onMouseEnter={() => setHoveredButton('close')}
+                                    onMouseLeave={() => setHoveredButton(null)}
+                                    style = {styles.modalContentBottom}>
+                                        <Text style={hoveredButton === 'close' 
+                                            ? styles.hoveredText : null}>Close</Text>
+                                </Pressable>
+                                <Pressable 
+                                    onPress={() => { 
+                                        setCardSet([...cardSet, <NewCard key={id}/>])
+                                        setId(id + 1)
+
+                                    }}
+                                    onMouseEnter={() => setHoveredButton('add')}
+                                    onMouseLeave={() => setHoveredButton(null)}
+                                    style = {styles.modalContentBottom}>
+                                        <Text style={hoveredButton === 'add' 
+                                            ? styles.hoveredText : null}>+</Text>
+                                </Pressable>
+                                <Pressable 
+                                    onPress={() => { 
                                         navigation.navigate('CardSet')
                                         setVisible(!visible)
                                     }}
-                                >
-                                    <Text>Create</Text>
-                                </TouchableOpacity>
+                                    onMouseEnter={() => setHoveredButton('create')}
+                                    onMouseLeave={() => setHoveredButton(null)}
+                                    style = {styles.modalContentBottom}>
+                                        <Text style={hoveredButton === 'create' 
+                                            ? styles.hoveredText : null}>Create</Text>
+                                </Pressable>
+
                             </View>
-                            <Pressable 
-                                onPress={() => {setVisible(!visible)}}
-                                style = {styles.modalClose}>
-                                    <Text>Close</Text>
-                            </Pressable>
                         </View>
                     </View>
                 </Modal>
-                </SafeAreaView>
+            </SafeAreaView>
         </SafeAreaProvider>
     );
 };
@@ -68,8 +118,42 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    modalClose:{
-        marginBottom: 10
+    modalBottom:{
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '100%',
+    },
+    modalContentBottom:{
+        justifyContent: 'center', 
+        flex: 1, 
+        alignItems: 'center', 
+    },
+    hoveredText: {
+        textDecorationLine: 'underline',
+    },
+    newCardStyle:{
+        height: 100,
+        width: windowWidth * 0.75,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        backgroundColor: 'grey',
+        borderRadius: 3,
+        margin: 5,
+    },
+    textInputStyle: {
+        width: '90%', // Make the text input take the full width
+        margin: 10, // Add some space between the inputs
+        padding: 10, // Add padding inside the input
+        backgroundColor: 'grey', // Set background color to white
+        borderRadius: 5, // Add border radius for better appearance
+    },
+    deleteStyle: {
+        backgroundColor: 'red',
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width:60
     }
 });
 
